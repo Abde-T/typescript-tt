bed4aedcffd6d255f8036ab37085a9931d78511b
Abderrahmane <tiamani3939@gmail.com>
2024-02-03 16:42:27 +0100

    first commit

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,39 @@
 var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-})
+var age = 30;
+var isBlackBelt = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(10));
+//Arrays
+var names = ['luigi', 'mario', 'yoshi', 10]; //if you only define one type in the array they you can only add or change the same type to it
+names.push('josh');
+names.push(2);
+//Objects
+var ninja = {
+    name: 'jhon',
+    belt: 'black',
+    age: 50,
+};
+ninja['rank'] = 'master';
+ninja = {
+    name: 'mark',
+    belt: 'white',
+    age: 20,
+};
+console.log(ninja);
+//explicit types
+var char;
+var agee;
+var blackBelt;
+//arrays
+var words = [];
+var numbers = [];
+//union types
+var x;
+var y;
+//objects
+var NinjaType;
+//any
+var a;
+var b;
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,53 @@
-const character = 'mario';
+let character = 'mario';
+let age = 30
+let isBlackBelt = false
 
-console.log(character);
+const circ = (diameter:number) => {
+  return diameter * Math.PI 
+}
 
-const inputs = document.querySelectorAll('input');
+console.log(circ(10))
 
-inputs.forEach(input => {
-  console.log(input);
-});
+//Arrays
+
+let names = ['luigi', 'mario', 'yoshi', 10] //if you only define one type in the array they you can only add or change the same type to it
+names.push('josh')
+names.push(2)
+
+//Objects
+let ninja = {
+  name:'jhon',
+  belt:'black',
+  age:50,
+}
+ninja['rank']='master'
+
+ninja = {
+  name:'mark',
+  belt:'white',
+  age:20,
+}
+
+console.log(ninja)
+
+
+//explicit types
+let char:string;
+let agee:number;
+let blackBelt:boolean;
+
+//arrays
+let words: string[] = [];
+let numbers: number[] = [];
+
+//union types
+let x : string | number 
+let y: (string|number|boolean)[];
+
+//objects
+let NinjaType: {name:string ,belt:string ,age:number};
+
+//any
+
+let a : any
+let b : any[]
